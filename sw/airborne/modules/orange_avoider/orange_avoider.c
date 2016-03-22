45e59adb2df69b73bb712da58362b349428f4af0
CharlesDelft <charles.lup@gmx.de>
2016-03-22 15:42:19 +0100

    Jerry

--- sw/airborne/modules/orange_avoider/orange_avoider.c
+++ sw/airborne/modules/orange_avoider/orange_avoider.c
@@ -11,7 +11,12 @@
  */
 
 #include "modules/orange_avoider/orange_avoider.h"
+
+/**
+* Charles 
+*/
 //#include "modules/computer_vision/colorfilter.h"
+
 #include "modules/ext_colorfilter/ext_colorfilter.h"
 #include "firmwares/rotorcraft/navigation.h"
 #include "state.h"
@@ -20,6 +25,10 @@
 
 uint8_t safeToGoForwards=FALSE;
 
+//int tresholdColorCount = 1000; //was 200
+/**
+* Charles 
+*/
 // Set all tresholds!!!
 float tresholdOrange_lcnt = 0.5;
 float tresholdOrange_clcnt = 0.5;
@@ -30,6 +39,8 @@ float tresholdavg_clcnt = 0.75;
 float tresholdavg_crcnt = 0.75;
 float tresholdavg_rcnt = 0.5;
 int32_t incrementForAvoidance;
+uint8_t stopGoingHigher=FALSE;
+float maxHeight = 6;
 
 void orange_avoider_init() {
 	// Initialise the variables of the colorfilter to accept orange
@@ -60,6 +71,8 @@ void orange_avoider_periodic() {
 		)		
 	 );  
 	printf("Save to go:%d \n avgleft= %3f, avgleftc= %3f, avgrightc= %3f, avgright= %3f \n oraleft %3f, oraleftc %3f, orarightc, %3f, oraright %3f \n", safeToGoForwards, avg_lcnt,avg_clcnt,avg_crcnt,avg_rcnt, lcnt, clcnt, crcnt, rcnt);
+
+	checkHeight(maxHeight);
 }
 
 
@@ -73,6 +86,38 @@ uint8_t increase_nav_heading(int32_t *heading, int32_t increment)
   INT32_ANGLE_NORMALIZE(*heading); // HEADING HAS INT32_ANGLE_FRAC....
   return FALSE;
 }
+
+
+
+
+uint8_t change_waypoint_random_inside_obstacle(uint8_t waypoint)
+{
+	struct EnuCoor_i new_coor;
+	struct EnuCoor_i *pos = stateGetPositionEnu_i(); // Get your current position
+	float maxy = 51.990657;
+	float miny = 51.990601;
+	float minx = 4.376748;
+	float maxx = 4.376845;
+	
+	srand(time(NULL));
+	
+	
+	// Get random number
+ 	float ry = (float)rand()*(maxy-miny)+miny;
+	float rx = (float)rand()*(maxx-minx)+minx;
+
+	// determine the random place of the waypoint inside the obstacle zone
+	
+	new_coor.x = rx;
+	new_coor.y = ry;
+	new_coor.z = pos->z; // Keep the height the same	
+
+	// Set the waypoint to the calculated position
+	waypoint_set_xy_i(waypoint, new_coor.x, new_coor.y);
+	
+  return FALSE;
+}
+
 uint8_t moveWaypointForwards(uint8_t waypoint, float distanceMeters){
 	  struct EnuCoor_i new_coor;
 	  struct EnuCoor_i *pos = stateGetPositionEnu_i(); // Get your current position
@@ -92,15 +137,45 @@ uint8_t moveWaypointForwards(uint8_t waypoint, float distanceMeters){
 	  return FALSE;
 }
 
+
+
 uint8_t chooseRandomIncrementAvoidance(){
 
-	int r = rand() % 2;
+	int r = rand() % 4;
 	if(r==0){
-		incrementForAvoidance=350;
+		incrementForAvoidance=1000;
+	}
+	else if(r==1){
+		incrementForAvoidance=2000;
+	}
+	else if(r==2){
+		incrementForAvoidance=-2000; //was 350
 	}
 	else{
-		incrementForAvoidance=-350;
+		incrementForAvoidance=-1000; // was -350
 	}
+	return FALSE;
+}
+
+float checkHeight(float maxHeight){
+
+	  //struct UtmCoor_f *pos = stateGetPositionUtm_f(); // Get your current position
+	  struct EnuCoor_f *pos3 = stateGetPositionEnu_f(); //
+	  //struct EcefCoor_f *pos2 = stateGetPositionEcef_f();
+
+	  //float altitude = pos->alt;
+	  //float altitude2 = pos2->y;
+	  float altitude3 = pos3->z;
+
+	  	  printf("Checking what the z coordinate is %f maxHeight: %f \n", altitude3, maxHeight);
+	  	  if(altitude3 > maxHeight){
+	  	  		stopGoingHigher=TRUE;
+	  	  	  }
+	  	  	  else{
+	  	  	  	stopGoingHigher=FALSE;
+	  	            }
+
+	  	printf("Is maxheight?  %d \n", stopGoingHigher);
 	return FALSE;
 }
 
--- sw/airborne/modules/orange_avoider/orange_avoider.h
+++ sw/airborne/modules/orange_avoider/orange_avoider.h
@@ -15,12 +15,16 @@
 #include <inttypes.h>
 
 extern uint8_t safeToGoForwards;
+extern uint8_t stopGoingHigher;
 extern int32_t incrementForAvoidance;
 extern void orange_avoider_init(void);
 extern void orange_avoider_periodic(void);
 extern uint8_t moveWaypointForwards(uint8_t waypoint, float distanceMeters);
 extern uint8_t increase_nav_heading(int32_t *heading, int32_t increment);
 extern uint8_t chooseRandomIncrementAvoidance(void);
+
+extern uint8_t change_waypoint_random_inside_obstacle(uint8_t waypoint);
+extern float checkHeight(float maxHeight);
 
 #endif
 
